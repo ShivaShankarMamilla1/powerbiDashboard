5c840a921c07a0839bfccd887ca4be21d210b3cb
ShivaShankarMamilla1 <shiva.mamilla@aifalabs.com>
2023-06-22 15:25:46 +0530

    latest dashboard changes

--- src/App.js
+++ src/App.js
@@ -2,19 +2,20 @@
 import './App.css';
 import { PowerBIEmbed } from 'powerbi-client-react';
 import { models } from "powerbi-client"
+import Grid from '@mui/material/Grid';
 
 
 function App() {
   return (
-    <div className="App">
+    <div className="App" style={{ display: "flex", flexDirection: "row" }}>
 
       <PowerBIEmbed
         embedConfig={{
           type: 'report',   // Supported types: report, dashboard, tile, visual, qna, paginated report and create
-          id: "1c7c5913-f2fd-4fb3-b89f-8a65e1dc1d6a",
-          embedUrl: "https://app.powerbi.com/reportEmbed?reportId=1c7c5913-f2fd-4fb3-b89f-8a65e1dc1d6a&groupId=4b39d1e5-8d21-45df-be7b-9e702135acc6&w=2&config=eyJjbHVzdGVyVXJsIjoiaHR0cHM6Ly9XQUJJLVVTLUNFTlRSQUwtQi1QUklNQVJZLXJlZGlyZWN0LmFuYWx5c2lzLndpbmRvd3MubmV0IiwiZW1iZWRGZWF0dXJlcyI6eyJtb2Rlcm5FbWJlZCI6dHJ1ZSwidXNhZ2VNZXRyaWNzVk5leHQiOnRydWV9fQ%3d%3d",
+          id: "766dae3c-ac8a-464a-88a4-0cefd2115e43",
+          embedUrl: "https://app.powerbi.com/reportEmbed?reportId=766dae3c-ac8a-464a-88a4-0cefd2115e43&groupId=4b39d1e5-8d21-45df-be7b-9e702135acc6&w=2&config=eyJjbHVzdGVyVXJsIjoiaHR0cHM6Ly9XQUJJLVVTLUNFTlRSQUwtQi1QUklNQVJZLXJlZGlyZWN0LmFuYWx5c2lzLndpbmRvd3MubmV0IiwiZW1iZWRGZWF0dXJlcyI6eyJtb2Rlcm5FbWJlZCI6dHJ1ZSwidXNhZ2VNZXRyaWNzVk5leHQiOnRydWV9fQ%3d%3d",
 
-          accessToken: "eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiIsIng1dCI6Ii1LSTNROW5OUjdiUm9meG1lWm9YcWJIWkdldyIsImtpZCI6Ii1LSTNROW5OUjdiUm9meG1lWm9YcWJIWkdldyJ9.eyJhdWQiOiJodHRwczovL2FuYWx5c2lzLndpbmRvd3MubmV0L3Bvd2VyYmkvYXBpIiwiaXNzIjoiaHR0cHM6Ly9zdHMud2luZG93cy5uZXQvM2ZmN2QzNWUtNzI2MS00NWIyLWEzMTQtZGYwODRlOWEyMWI3LyIsImlhdCI6MTY4NzM0ODU2MywibmJmIjoxNjg3MzQ4NTYzLCJleHAiOjE2ODczNTMwNDQsImFjY3QiOjAsImFjciI6IjEiLCJhaW8iOiJBVFFBeS84VEFBQUFCVnJ3WmZzM1FSa3N3dG1nTVdQbUJ4NWZULy9MM2gwRWxMVVN1eUN1SlgvaTdjOSt5R3h5amRTYlB5TGg3VlI2IiwiYW1yIjpbInB3ZCJdLCJhcHBpZCI6Ijg3MWMwMTBmLTVlNjEtNGZiMS04M2FjLTk4NjEwYTdlOTExMCIsImFwcGlkYWNyIjoiMCIsImZhbWlseV9uYW1lIjoiTWFtaWxsYSIsImdpdmVuX25hbWUiOiJTaGl2YSBTaGFua2FyIiwiaXBhZGRyIjoiMTgzLjgyLjEyNS40MyIsIm5hbWUiOiJTaGl2YSBTaGFua2FyIE1hbWlsbGEiLCJvaWQiOiIyMzE3ODEzMS02NTQ2LTQ1ZTktOTI0ZC0zMjRmNmY2MGZlMWEiLCJwdWlkIjoiMTAwMzIwMDI5MDZGOTI4MyIsInB3ZF9leHAiOiIzMTE2OTYzMDYiLCJwd2RfdXJsIjoiaHR0cHM6Ly9wcm9kdWN0aXZpdHkuc2VjdXJlc2VydmVyLm5ldC9taWNyb3NvZnQ_bWFya2V0aWQ9ZW4tVVNcdTAwMjZlbWFpbD1zaGl2YS5tYW1pbGxhJTQwYWlmYWxhYnMuY29tXHUwMDI2c291cmNlPVZpZXdVc2Vyc1x1MDAyNmFjdGlvbj1SZXNldFBhc3N3b3JkIiwicmgiOiIwLkFYMEFYdFAzUDJGeXNrV2pGTjhJVHBvaHR3a0FBQUFBQUFBQXdBQUFBQUFBQUFDYUFHWS4iLCJzY3AiOiJ1c2VyX2ltcGVyc29uYXRpb24iLCJzaWduaW5fc3RhdGUiOlsia21zaSJdLCJzdWIiOiJHU3Rtdm9VbTgwLUwwTlR6RnRGbUJBdHpzZE1XWHhvNFh5U2hPY0xaN09BIiwidGlkIjoiM2ZmN2QzNWUtNzI2MS00NWIyLWEzMTQtZGYwODRlOWEyMWI3IiwidW5pcXVlX25hbWUiOiJzaGl2YS5tYW1pbGxhQGFpZmFsYWJzLmNvbSIsInVwbiI6InNoaXZhLm1hbWlsbGFAYWlmYWxhYnMuY29tIiwidXRpIjoiNzUwSVRVYml2a3FmU3FVZ0J5QVRBQSIsInZlciI6IjEuMCIsIndpZHMiOlsiYjc5ZmJmNGQtM2VmOS00Njg5LTgxNDMtNzZiMTk0ZTg1NTA5Il19.erwCAXAoPjT6MFhG8ofRh63BhAjNy89ryqw1lsMrboDhVCD9rzw-8BLbkrn684Nlx1Xu4_bCd220P8ckdgxDKAWlH7iRCqwWODIZqr32Lsw8cjqn3dRvEPldq5PGgyBGFF19DhBt2Gym4OaXfG-_eki8UHPESGAwrWXEXFGcJZ5fByIjrdxB0YgIpfDN2dZAUCjbB6GqqlMPIBfagS4fNXDKSFFan2457XMCDJRaDDVnFU6GPO0COrfQRAYRlW-yk6WRrhBvfcsbESfLwfee-r21ocDUqQ0hfjwk1fWUkDpMqdrYUpznzB2fSR028VIkkbKS4O_MSQkVwsKGncqdaQ",
+          accessToken: "eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiIsIng1dCI6Ii1LSTNROW5OUjdiUm9meG1lWm9YcWJIWkdldyIsImtpZCI6Ii1LSTNROW5OUjdiUm9meG1lWm9YcWJIWkdldyJ9.eyJhdWQiOiJodHRwczovL2FuYWx5c2lzLndpbmRvd3MubmV0L3Bvd2VyYmkvYXBpIiwiaXNzIjoiaHR0cHM6Ly9zdHMud2luZG93cy5uZXQvM2ZmN2QzNWUtNzI2MS00NWIyLWEzMTQtZGYwODRlOWEyMWI3LyIsImlhdCI6MTY4NzQyNjIxNiwibmJmIjoxNjg3NDI2MjE2LCJleHAiOjE2ODc0MzA4NDEsImFjY3QiOjAsImFjciI6IjEiLCJhaW8iOiJBVFFBeS84VEFBQUFQc1p6S1p0MGd0YUI3QUFzTTNJV25XV083cW44T3NaaEhma1NIT3pETzlWUUMzR2duVkhCR1IrYktWcnNzb01GIiwiYW1yIjpbInB3ZCJdLCJhcHBpZCI6Ijg3MWMwMTBmLTVlNjEtNGZiMS04M2FjLTk4NjEwYTdlOTExMCIsImFwcGlkYWNyIjoiMiIsImZhbWlseV9uYW1lIjoiTWFtaWxsYSIsImdpdmVuX25hbWUiOiJTaGl2YSBTaGFua2FyIiwiaXBhZGRyIjoiMTgzLjgyLjEyNS40MyIsIm5hbWUiOiJTaGl2YSBTaGFua2FyIE1hbWlsbGEiLCJvaWQiOiIyMzE3ODEzMS02NTQ2LTQ1ZTktOTI0ZC0zMjRmNmY2MGZlMWEiLCJwdWlkIjoiMTAwMzIwMDI5MDZGOTI4MyIsInJoIjoiMC5BWDBBWHRQM1AyRnlza1dqRk44SVRwb2h0d2tBQUFBQUFBQUF3QUFBQUFBQUFBQ2FBR1kuIiwic2NwIjoidXNlcl9pbXBlcnNvbmF0aW9uIiwic2lnbmluX3N0YXRlIjpbImttc2kiXSwic3ViIjoiR1N0bXZvVW04MC1MME5UekZ0Rm1CQXR6c2RNV1h4bzRYeVNoT2NMWjdPQSIsInRpZCI6IjNmZjdkMzVlLTcyNjEtNDViMi1hMzE0LWRmMDg0ZTlhMjFiNyIsInVuaXF1ZV9uYW1lIjoic2hpdmEubWFtaWxsYUBhaWZhbGFicy5jb20iLCJ1cG4iOiJzaGl2YS5tYW1pbGxhQGFpZmFsYWJzLmNvbSIsInV0aSI6Im1WQkFub0hPajBLSlhqTHdzVGM4QUEiLCJ2ZXIiOiIxLjAiLCJ3aWRzIjpbImI3OWZiZjRkLTNlZjktNDY4OS04MTQzLTc2YjE5NGU4NTUwOSJdfQ.RjCxn6P6Dbjjy1Uf1tvZ59D_ccrw5T4pLYk-0M90gRKELPF9AVAjfUegQX6UIWf7LQJSYO4ro6EA0W9ff49fIb5fMZKa6lRucZ_F9Do6OS7xWRx5exL-bRt-FaFZ8nGN6afDXWfcmGzGo2ReaQ5oGGOKIuNX8019-gGe1L6d2-vfrfSdcs14-U_-o-F7BTX9hoItzugsL0-9m6SqaAWqvlTFcBIOQIuE8UoIODFuWy8SIEWpf6jB8zOScqMed-wXwNvTyOHrDlNGFK_ivJ1w2a-t7avz4guiVFijR2jTn7R2AYU2WiJmDxyNOoZVTYnGUckZEO8O9_p5fEKLDtsQew",
           // accessToken: "access-token",
           tokenType: models.TokenType.Aad, // Use models.TokenType.Aad for SaaS embed
           settings: {
@@ -47,9 +48,10 @@ function App() {
 
 
       />
-      <div style={{ display: "flex", flexDirection: "row", justifyContent: "flex-end" }}>
-        <img src="https://img.freepik.com/premium-vector/robot-icon-chat-bot-sign-support-service-concept-chatbot-character-flat-style_41737-796.jpg" style={{ height: "150px", marginLeft: "auto" }} alt="icon" />
+      <div style={{ position: "absolute", top: "550px", left: "1300px" }}>
+        <img src="https://img.freepik.com/premium-vector/robot-icon-chat-bot-sign-support-service-concept-chatbot-character-flat-style_41737-796.jpg" style={{ height: "150px" }} alt="icon" />
       </div>
+
     </div>
   );
 }
